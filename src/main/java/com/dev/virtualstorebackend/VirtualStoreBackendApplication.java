4e55cf57a03d31347b283a74530eb1d956a72dfa
danielGoHorse <dannysivle@gmail.com>
2024-02-22 15:26:24 -0300

    first commit

---
+++ src/main/java/com/dev/virtualstorebackend/VirtualStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.virtualstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/virtualstorebackend/VirtualStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.virtualstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
